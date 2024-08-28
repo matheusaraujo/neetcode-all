207f1165791000da51356391aa152f9a5c745423
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-28 12:02:55 -0300

    feat(heap): 1046.last-stone-weight.py

---
+++ 1046.last-stone-weight.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=1046 lang=python3
+#
+# [1046] Last Stone Weight
+#
+
+# @lc code=start
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            x = heapq.heappop(stones)
+            y = heapq.heappop(stones)
+            if y > x:
+                heapq.heappush(stones, x - y)
+
+        return -stones[0] if len(stones) else 0
+
+
+# @lc code=end
+
