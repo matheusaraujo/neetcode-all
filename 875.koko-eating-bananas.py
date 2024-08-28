aa7073e21a17ea9357b2550b16b873b147bccdcd
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-28 15:51:49 -0300

    feat(binary-search): 875.koko-eating-bananas.py

---
+++ 875.koko-eating-bananas.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=875 lang=python3
+#
+# [875] Koko Eating Bananas
+#
+
+# @lc code=start
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left, right = 1, max(piles)
+        result = right
+
+        while left <= right:
+            k = (left + right) // 2
+
+            totalTime = sum(math.ceil((float(p) / k)) for p in piles)
+            if totalTime <= h:
+                result = k
+                right = k - 1
+            else:
+                left = k + 1
+
+        return result
+
+# @lc code=end
+
