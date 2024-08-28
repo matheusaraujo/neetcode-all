39f2b369620aa7323e84091a6c05c8f278aaf6ff
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-28 11:32:01 -0300

    feat(trees): 543.diameter-of-binary-tree.py

---
+++ 543.diameter-of-binary-tree.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=543 lang=python3
+#
+# [543] Diameter of Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.result = 0
+
+        def dfs(node: Optional[TreeNode]):
+            if not node: return 0
+            left, right = dfs(node.left), dfs(node.right)
+            self.result = max(self.result, left + right)
+
+            return max(left, right) + 1
+
+        dfs(root)
+
+        return self.result
+
+# @lc code=end
+
