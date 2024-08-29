b77a188e016e41c1bf29aa1c7657bfea92d382cc
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-28 21:18:05 -0300

    feat(advanced-graphs): 787.cheapest-flights-within-k-stops.py

---
+++ 787.cheapest-flights-within-k-stops.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=787 lang=python3
+#
+# [787] Cheapest Flights Within K Stops
+#
+
+# @lc code=start
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        prices = [math.inf] * n
+        prices[src] = 0
+
+        for i in range(k + 1):
+            tmpPrices = prices.copy()
+
+            for source, dest, price in flights:
+                if prices[source] == math.inf: continue
+                tmpPrices[dest] = min(tmpPrices[dest], prices[source] + price)
+
+            prices = tmpPrices
+
+        return -1 if prices[dst] == math.inf else prices[dst]
+
+
+# @lc code=end
+
