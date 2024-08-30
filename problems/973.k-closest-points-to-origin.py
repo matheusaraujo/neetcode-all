244b92a5058aae730a43a24141276e2e5935d386
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-30 17:53:38 -0300

    feat(heap): 973.k-closest-points-to-origin.py

---
+++ problems/973.k-closest-points-to-origin.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=973 lang=python3
+#
+# [973] K Closest Points to Origin
+#
+
+# @lc code=start
+class Solution:
+    def __buildMinHeap(self, points: List[List[int]]) -> List[Tuple[int, int, int]]:
+        minHeap = []
+        for x, y in points:
+            dist = (x ** 2) + (y ** 2)
+            minHeap.append((dist, x, y))
+        heapq.heapify(minHeap)
+        return minHeap
+
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        minHeap, result = self.__buildMinHeap(points), []
+
+        for _ in range(k):
+            _, x, y = heapq.heappop(minHeap)
+            result.append((x, y))
+
+        return result
+
+
+# @lc code=end
+
