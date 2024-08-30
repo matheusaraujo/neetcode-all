764e5f318bf94414007c74ddb8f1a336a4c77d01
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-30 14:04:12 -0300

    feat(linked-list): 138.copy-list-with-random-pointer.py

---
+++ 138.copy-list-with-random-pointer.py
@@ -0,0 +1,37 @@
+#
+# @lc app=leetcode id=138 lang=python3
+#
+# [138] Copy List with Random Pointer
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        copyMap = {None: None}
+
+        curr = head
+        while curr:
+            copy = Node(curr.val)
+            copyMap[curr] = copy
+            curr = curr.next
+
+        curr = head
+        while curr:
+            copy = copyMap[curr]
+            copy.next = copyMap[curr.next]
+            copy.random = copyMap[curr.random]
+            curr = curr.next
+
+        return copyMap[head]
+
+# @lc code=end
+
