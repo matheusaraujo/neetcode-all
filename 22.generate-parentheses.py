2c3d753ac98b4d83c9800c161c1b42c638f8e2e8
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-29 21:57:48 -0300

    feat(stack): 22.generate-parentheses.py

---
+++ 22.generate-parentheses.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=22 lang=python3
+#
+# [22] Generate Parentheses
+#
+
+# @lc code=start
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+
+        def solve(stack, open, close):
+            if open == close == n:
+                result.append("".join(stack))
+                return
+
+            if open < n:
+                stack.append("(")
+                solve(stack, open + 1, close)
+                stack.pop()
+
+            if close < open:
+                stack.append(")")
+                solve(stack, open, close + 1)
+                stack.pop()
+
+        solve([], 0, 0)
+        return result
+
+
+# @lc code=end
+
