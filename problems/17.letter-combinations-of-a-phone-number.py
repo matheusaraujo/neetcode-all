fd21a6707f8b1ce9d411f9129652bb4585f62d22
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-31 11:01:59 -0300

    feat(backtracking): 17.letter-combinations-of-a-phone-number.py

---
+++ problems/17.letter-combinations-of-a-phone-number.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=17 lang=python3
+#
+# [17] Letter Combinations of a Phone Number
+#
+
+# @lc code=start
+class Solution:
+    digitToChar = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz"
+    }
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        result = []
+
+        def backtrack(i, curr):
+            if len(curr) == len(digits):
+                result.append(curr)
+                return
+            for c in self.digitToChar[digits[i]]:
+                backtrack(i + 1, curr + c)
+
+        if digits: backtrack(0, "")
+
+        return result
+
+# @lc code=end
+
