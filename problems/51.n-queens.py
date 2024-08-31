f16accf2ca08d123d5a8711362d55e875954f12a
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-31 11:31:43 -0300

    feat(backtracking): 51.n-queens.py

---
+++ problems/51.n-queens.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode id=51 lang=python3
+#
+# [51] N-Queens
+#
+
+# @lc code=start
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        columns, positiveDiagonal, negativeDiagonal = set(), set(), set()
+        result, board = [], [["."] * n for i in range(n)]
+
+        def backtrack(row):
+            if row == n:
+                result.append(["".join(r) for r in board])
+                return
+
+            for col in range(n):
+                if (col in columns
+                    or (row + col) in positiveDiagonal
+                    or (row - col) in negativeDiagonal
+                ): continue
+
+                columns.add(col)
+                positiveDiagonal.add(row + col)
+                negativeDiagonal.add(row - col)
+                board[row][col] = "Q"
+
+                backtrack(row + 1)
+
+                columns.remove(col)
+                positiveDiagonal.remove(row + col)
+                negativeDiagonal.remove(row - col)
+                board[row][col] = "."
+
+        backtrack(0)
+
+        return result
+
+# @lc code=end
+
