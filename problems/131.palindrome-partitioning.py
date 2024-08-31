bac75bcd38eeccd90cb9dd21043d6c26b17553f7
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-31 10:56:27 -0300

    feat(backtracking): 131.palindrome-partitioning.py

---
+++ problems/131.palindrome-partitioning.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=131 lang=python3
+#
+# [131] Palindrome Partitioning
+#
+
+# @lc code=start
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        result, partition = [], []
+
+        def isPalindrome(i: int, j: int):
+            while i < j:
+                if s[i] != s[j]: return False
+                i, j = i + 1, j - 1
+            return True
+
+        def backtracking(i):
+            if i >= len(s):
+                result.append(partition[:])
+                return
+
+            for j in range(i, len(s)):
+                if isPalindrome(i, j):
+                    partition.append(s[i:j+1])
+                    backtracking(j + 1)
+                    partition.pop()
+
+        backtracking(0)
+
+        return result
+
+# @lc code=end
+
