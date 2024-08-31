e6b7d0a63b9cc8406256eac1d15f6a1c26c2005b
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-31 11:55:49 -0300

    feat(math-geometry): 2013.detect-squares.py

---
+++ problems/2013.detect-squares.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=2013 lang=python3
+#
+# [2013] Detect Squares
+#
+
+# @lc code=start
+class DetectSquares:
+
+    def __init__(self):
+        self.pointsCount = defaultdict(int)
+        self.points = []
+
+    def add(self, point: List[int]) -> None:
+        self.pointsCount[tuple(point)] += 1
+        self.points.append(point)
+
+    def count(self, point: List[int]) -> int:
+        result = 0
+        px, py = point
+
+        for x, y in self.points:
+            if abs(py - y) != abs(px - x) or x == px or y == py:
+                continue
+            result += self.pointsCount[(x, py)] * self.pointsCount[(px, y)]
+        return result
+
+
+
+# Your DetectSquares object will be instantiated and called as such:
+# obj = DetectSquares()
+# obj.add(point)
+# param_2 = obj.count(point)
+# @lc code=end
+
