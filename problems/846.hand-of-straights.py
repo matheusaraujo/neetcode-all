dc83b069cc6fe98b1846bb8463a07ef33dea02ac
Matheus Araujo <matheus.saraujo@gmail.com>
2024-09-02 19:11:32 -0300

    feat(greedy): 846.hand-of-straights.py

---
+++ problems/846.hand-of-straights.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=846 lang=python3
+#
+# [846] Hand of Straights
+#
+
+# @lc code=start
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize: return False
+
+        count = {}
+        for n in hand:
+            count[n] = 1 + count.get(n, 0)
+
+        minHeap = list(count.keys())
+        heapq.heapify(minHeap)
+
+        while minHeap:
+            first = minHeap[0]
+            for n in range(first, first + groupSize):
+                if n not in count: return False
+                count[n] -= 1
+                if count[n] == 0:
+                    if n != minHeap[0]: return False
+                    heapq.heappop(minHeap)
+
+        return True
+
+
+# @lc code=end
+
