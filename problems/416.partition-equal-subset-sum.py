24c8c4675cf35a7d5c4c1b83c6cfb5f602ece4d2
Matheus Araujo <matheus.saraujo@gmail.com>
2024-09-02 10:47:54 -0300

    feat(1d-dynamic-programming): 416.partition-equal-subset-sum.py

--- problems/416.partition-equal-subset-sum.py
+++ problems/416.partition-equal-subset-sum.py
@@ -9,17 +9,15 @@ class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         if sum(nums) % 2: return False
 
-        dp = set()
+        dp, target = set(), sum(nums) // 2
         dp.add(0)
-        target = sum(nums) // 2
 
-        for i in range(len(nums) -1, -1, -1):
-            nextDp = set()
+        for i in range(len(nums)):
+            newDp = set(dp)
             for t in dp:
                 if t + nums[i] == target: return True
-                nextDp.add(t + nums[i])
-                nextDp.add(t)
-            dp = nextDp
+                newDp.add(t + nums[i])
+            dp = newDp
         return False
 
 # @lc code=end
