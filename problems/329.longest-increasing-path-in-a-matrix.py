c3e3a32e78948875bbd0b320ceb552126dfbadb4
Matheus Araujo <matheus.saraujo@gmail.com>
2024-09-03 20:49:08 -0300

    feat(2d-dynamic-programming): 329.longest-increasing-path-in-a-matrix.py

---
+++ problems/329.longest-increasing-path-in-a-matrix.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=329 lang=python3
+#
+# [329] Longest Increasing Path in a Matrix
+#
+
+# @lc code=start
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        dp = {}
+
+        def dfs(row, col, val):
+            if row not in range(len(matrix)) \
+                or col not in range(len(matrix[0])) \
+                or matrix[row][col] <= val:
+                return 0
+
+            if (row, col) in dp: return dp[(row, col)]
+
+            result = 1
+            result = max(result, 1 + dfs(row + 1, col, matrix[row][col]))
+            result = max(result, 1 + dfs(row, col + 1, matrix[row][col]))
+            result = max(result, 1 + dfs(row - 1, col, matrix[row][col]))
+            result = max(result, 1 + dfs(row, col - 1, matrix[row][col]))
+            dp[(row, col)] = result
+            return result
+
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                dfs(row, col, -1)
+
+        return max(dp.values())
+
+# @lc code=end
+
