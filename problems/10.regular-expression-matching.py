5936e970ad6f9d39de6f8fef8104d2b083958615
Matheus Araujo <matheus.saraujo@gmail.com>
2024-09-03 21:46:54 -0300

    feat(2d-dynamic-programming): 10.regular-expression-matching.py

---
+++ problems/10.regular-expression-matching.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=10 lang=python3
+#
+# [10] Regular Expression Matching
+#
+
+# @lc code=start
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = [[False] * (len(p) + 1) for i in range(len(s) + 1)]
+        dp[len(s)][len(p)] = True
+
+        for i in range(len(s), -1, -1):
+            for j in range(len(p) - 1, -1, -1):
+                m = i < len(s) and (s[i] == p[j] or p[j] == ".")
+
+                if j + 1 < len(p) and p[j + 1] == "*":
+                    dp[i][j] = dp[i][j + 2]
+                    if m: dp[i][j] = dp[i + 1][j] or dp[i][j]
+                elif m: dp[i][j] = dp[i + 1][j + 1]
+
+        return dp[0][0]
+
+# @lc code=end
+
