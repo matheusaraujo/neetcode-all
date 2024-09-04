a553c23ac3fe806514b6caa547ecf7aeddebdf4d
Matheus Araujo <matheus.saraujo@gmail.com>
2024-09-04 19:17:50 -0300

    feat(backtracking): 306.additive-number.py [extras]

---
+++ extras/306.additive-number.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=306 lang=python3
+#
+# [306] Additive Number
+#
+
+# @lc code=start
+class Solution:
+    def isAdditiveNumber(self, num: str) -> bool:
+        def solve(num: str, idx: int, first: int, second: int, numNumbers: int) -> bool:
+            if idx == len(num): return numNumbers >= 3
+            currNum = 0
+            for i in range(idx, len(num)):
+                currNum = currNum * 10 + (ord(num[i]) - ord("0"))
+                if first == None or second == None or currNum == first + second:
+                    if solve(num, i + 1, second, currNum, numNumbers + 1):
+                        return True
+                if num[idx] == "0": break
+            return False
+
+        return solve(num, 0, None, None, 0)
+
+
+# @lc code=end
+
