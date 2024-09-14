b4afd32edabc350b91e22ffb9e83ea93b6d7d8d0
Matheus Araujo <matheus.saraujo@gmail.com>
2024-09-14 10:55:46 -0300

    feat(arrays-hashing): 1299.replace-elements-with-greatest-element-on-right-side.py

---
+++ problems/1299.replace-elements-with-greatest-element-on-right-side.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=1299 lang=python3
+#
+# [1299] Replace Elements with Greatest Element on Right Side
+#
+
+# @lc code=start
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        rightMax = -1
+        for i in range(len(arr) - 1, -1, -1):
+            newMax = max(rightMax, arr[i])
+            arr[i], rightMax = rightMax, newMax
+
+        return arr
+
+# @lc code=end
+
