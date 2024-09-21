1f97ecd76bf4efaae852e706e629c90abfc6e141
Matheus Araujo <matheus.saraujo@gmail.com>
2024-09-21 08:27:52 -0300

    feat(arrays-hashing): 205.isomorphic-strings.py

---
+++ problems/205.isomorphic-strings.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=205 lang=python3
+#
+# [205] Isomorphic Strings
+#
+
+# @lc code=start
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        map_st, map_ts = {}, {}
+        for cs, ct in zip(s, t):
+            if (cs in map_st and ct != map_st[cs]): return False
+            if (ct in map_ts and cs != map_ts[ct]): return False
+            map_st[cs] = ct
+            map_ts[ct] = cs
+
+        return True
+
+# @lc code=end
+
