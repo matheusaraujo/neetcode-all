bb317b9c3684d3aa6a01e9a0c8b09b64cbfa2f14
Matheus Araujo <matheus.saraujo@gmail.com>
2024-09-28 19:15:52 -0300

    feat(arrays-hashing): 496.next-greater-element-i.py

---
+++ problems/496.next-greater-element-i.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=496 lang=python3
+#
+# [496] Next Greater Element I
+#
+
+# @lc code=start
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        next_greater, stack = {}, []
+
+        for num in nums2:
+            while stack and num > stack[-1]:
+                next_greater[stack.pop()] = num
+            stack.append(num)
+
+        return [next_greater.get(num, -1) for num in nums1]
+
+# @lc code=end
+
