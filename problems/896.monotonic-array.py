109f4d9e4e08c4ebfbe929d435da124680362e69
Matheus Araujo <matheus.saraujo@gmail.com>
2024-10-09 09:06:53 -0300

    feat(arrays-hashing): 896.monotonic-array.py

---
+++ problems/896.monotonic-array.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=896 lang=python3
+#
+# [896] Monotonic Array
+#
+
+# @lc code=start
+# class Solution:
+#     def isMonotonic(self, nums: List[int]) -> bool:
+#         if len(nums) == 1: return True
+#         i = 1
+#         while i < len(nums) and nums[i] == nums[i - 1]:
+#             i += 1
+#         if i == len(nums): return True
+#         increasing = nums[i] > nums[i - 1]
+#         for i in range(i, len(nums)):
+#             if increasing and nums[i] < nums[i - 1]: return False
+#             elif not increasing and nums[i] > nums[i - 1]: return False
+#         return True
+
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        increase, decrease = True, True
+
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i - 1]: increase = False
+            if nums[i] > nums[i - 1]: decrease = False
+
+        return increase or decrease
+
+# @lc code=end
+
